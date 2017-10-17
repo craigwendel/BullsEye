caea083beb918bf44ce1117752b7d1a4c4aa4e7b
Craig Wendel <wendelcraig@gmail.com>
2017-10-17 13:24:50 -0500

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Craig Wendel on 10/16/17.
+//  Copyright © 2017 Craig Wendel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController (
+            title: "Hello World",
+            message: "This is my first app",
+            preferredStyle: .alert
+        )
+
+        let action = UIAlertAction (
+            title: "Awesome",
+            style: .default,
+            handler: nil
+        )
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+
+}
+
