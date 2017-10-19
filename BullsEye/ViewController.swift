7306c2bfc438afdc452171dcce2200160313855e
Craig Wendel <wendelcraig@gmail.com>
2017-10-18 23:05:03 -0500

    Add logic for total points and rounds. Add game reset. Add images to UI and code for slider images. Add view controller for About page.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,14 +12,35 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var totalScore = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        startNewRound()
+        
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable =
+            trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
+        startNewGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,10 +49,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\n The target value is \(targetValue)"
         
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        totalScore += points
+        
+        let message = "Your guess: \(currentValue)" + "\nYou scored \(points) points!"
         let alert = UIAlertController (
-            title: "Your Guess",
+            title: title,
             message: message,
             preferredStyle: .alert
         )
@@ -39,13 +80,12 @@ class ViewController: UIViewController {
         let action = UIAlertAction (
             title: "OK",
             style: .default,
-            handler: nil
+            handler: { action in self.startNewRound() }
         )
 
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -53,14 +93,23 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
         updateLabels()
     }
     
+    @IBAction func startNewGame() {
+       totalScore = 0
+        round = 0
+        startNewRound()
+    }
+    
     func updateLabels () {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(totalScore)
+        roundLabel.text = String(round)
     }
 
 }
